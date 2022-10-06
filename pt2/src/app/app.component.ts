f63225886da4d786a5d4c2a1f376dbd820ba2fcb
Andreu6 <suarezbellavistaandreu@gmail.com>
2022-10-06 19:19:30 +0200

    Pt2 feta

--- pt2/src/app/app.component.ts
+++ pt2/src/app/app.component.ts
@@ -7,6 +7,7 @@ agility: number;
 intelligence: number;
 life: number;
 editable?: boolean;
+valorBoton:string;
 }
 
 @Component({
@@ -17,31 +18,36 @@ styleUrls: ['./app.component.css']
 export class AppComponent {
 
 serverCharacters: Character[] = [];
-valorBoton:string = ""
+txt: String = "";
+
 
 constructor() {
 // Ejemplo de respuesta de un servidor en formato JSON
 const serverJson = `[
-{"name": "Jugger", "strength": 18, "agility": 12, "intelligence": 6, "life": 30, "editable":true },
-{"name": "Pelegrin", "strength": 20, "agility": 8, "intelligence": 6, "life": 40, "editable":true },
-{"name": "Dorthak", "strength": 12, "agility": 18, "intelligence": 10, "life": 16, "editable":true },
-{"name": "Kharak", "strength": 8, "agility": 20, "intelligence": 12, "life": 14, "editable":true },
-{"name": "Perz", "strength": 10, "agility": 6, "intelligence": 20, "life": 10, "editable":true }
+{"name": "Jugger", "strength": 18, "agility": 12, "intelligence": 6, "life": 30, "editable":true, "valorBoton": "Edit" },
+{"name": "Pelegrin", "strength": 20, "agility": 8, "intelligence": 6, "life": 40, "editable":true, "valorBoton": "Edit" },
+{"name": "Dorthak", "strength": 12, "agility": 18, "intelligence": 10, "life": 16, "editable":true, "valorBoton": "Edit" },
+{"name": "Kharak", "strength": 8, "agility": 20, "intelligence": 12, "life": 14, "editable":true, "valorBoton": "Edit" },
+{"name": "Perz", "strength": 10, "agility": 6, "intelligence": 20, "life": 10, "editable":true, "valorBoton": "Edit" }
 ]`;
 
 // Parseamos la información y la convertimos directamente en un array de "Character"
 this.serverCharacters = JSON.parse(serverJson);
 }
 
-cambiarBoton(Character:Character){
+cambiarBoton(Character:Character, txt:String):String {
 
 if(Character.editable == true){
 Character.editable = false;
+Character.valorBoton = "Save";
 
 } else if(Character.editable == false) {
 Character.editable = true;
+Character.valorBoton = "Edit";
 
 }
+
+return txt = JSON.stringify(Character);
 
 }
 
